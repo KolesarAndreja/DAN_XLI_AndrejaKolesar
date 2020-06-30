e6122319e94ddfe9d54fcea0a5bf3a3cddd0ea5b
KolesarAndreja <kolesar.andreja@gmail.com>
2020-06-30 08:35:11 +0200

    enable and disable buttons, add comment

--- DAN_XLI/MainWindow.xaml.cs
+++ DAN_XLI/MainWindow.xaml.cs
@@ -1,20 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace DAN_XLI
 {
@@ -65,29 +55,30 @@ namespace DAN_XLI
         #endregion
 
         #region Events of BackgroundWorker class
+        /// <summary>
+        /// The DoWork event handler is where the time consuming operation runs on the background thread
+        /// </summary>
         private void DoWork(object sender, DoWorkEventArgs e)
         {
             int sum = 0;
-            if (numberOfCopies != 0)
-            {
-                string fileName;
+            string fileName;
 
-                //last step will be bigger for this number if 100%numberOfCopies != 0
+                //last step will be larger for this number if 100%numberOfCopies != 0
                 int lastStep = 100 % numberOfCopies;
-             
+
                 int step = Convert.ToInt32(100 / numberOfCopies);
 
                 for (int i = 1; i <= numberOfCopies; i++)
                 {
                     Thread.Sleep(1000);
                     sum = sum + step;
-                    if(i == numberOfCopies)
+                    if (i == numberOfCopies)
                     {
                         sum += lastStep;
                     }
                     DateTime dateTime = DateTime.Now;
                     // Calling ReportProgress() method raises ProgressChanged event
-                    // To this method pass the percentage of processing that is complete
+                    //passing the percentage of processing that is complete
                     worker.ReportProgress(sum);
 
                     //create file with textInput content
@@ -108,28 +99,43 @@ namespace DAN_XLI
                         return;
                     }
                 }
-           
-            }
-            e.Result = sum;
-        }
+            // Store the result in Result property of DoWorkEventArgs object
+            e.Result = "Printing process is finished!";
+                
 
+        }
+        /// <summary>
+        /// This event gets raised for 3 reasons: 
+        /// 1.When the background worker has completed processing successfully
+        /// 2.When the background worker encountered an error
+        /// 3.When the background worker is requested to cancel the execution
+        /// </summary>
         private void RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
+            //3.
             if (e.Cancelled)
             {
-                label1.Content = "Processing cancelled";
+                label2.Content = "Printing cancelled";
+                btnCancel.IsEnabled = false;
             }
+            //2.
             else if (e.Error != null)
             {
                 label1.Content = e.Error.Message;
             }
+            //1.
             else
             {
                 label1.Content = e.Result.ToString();
+                btnCancel.IsEnabled = false;
+                label2.Content = null;
             }
 
         }
 
+        /// <summary>
+        /// The ProgressChanged event handler is where we write code to update the user interface elements with the progress made so far.
+        /// </summary>
         private void ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
             progressBar.Value = e.ProgressPercentage;
@@ -142,30 +148,49 @@ namespace DAN_XLI
         //get text input 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
+            //Enable or disable Print button depending on the textInput and numberOfCopies
             textInput = text.Text;
+            if (!String.IsNullOrEmpty(textInput) && numberOfCopies != 0)
+            {
+                btnPrint.IsEnabled = true;
+            }
+            else
+            {
+                btnPrint.IsEnabled = false;
+            }
+
         }
 
         //get copies input
         private void CurrentCopies_TextChanged(object sender, TextChangedEventArgs e)
         {
             copiesInput = copies.Text;
+            if (!String.IsNullOrEmpty(textInput) && numberOfCopies != 0)
+            {
+                btnPrint.IsEnabled = true;
+            }
+            else
+            {
+                btnPrint.IsEnabled = false;
+            }
+
         }
 
         private void BtnPrint_Click(object sender, RoutedEventArgs e)
         {
-    
-                // Check if the backgroundWorker is already busy running the asynchronous operation
-                if (!worker.IsBusy)
-                {
-                    // This method will start the execution asynchronously in the background
-                    worker.RunWorkerAsync();
-                }
-                else
-                {
-                    label2.Content = "Busy processing, please wait...";
-                }
-            
-            
+            // Check if the backgroundWorker is already busy running the asynchronous operation
+            if (!worker.IsBusy)
+            {
+                // This method will start the execution asynchronously in the background
+                worker.RunWorkerAsync();
+                btnCancel.IsEnabled = true;
+            }
+            else
+            {
+                label2.Content = "Printing in progress...";
+            }
+
+
         }
 
         private void BtnCancel_Click(object sender, RoutedEventArgs e)
@@ -175,16 +200,14 @@ namespace DAN_XLI
                 // Cancel the asynchronous operation if still in progress
                 worker.CancelAsync();
             }
-            else
-            {
-                label2.Content = "No operation in progress to cancel.";
-            }
+
         }
-        #endregion
 
         private void ProgressBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
 
         }
+        #endregion
+
     }
 }
